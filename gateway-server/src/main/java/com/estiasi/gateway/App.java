efe8abcf4a602d1ecf9cd4e1dc7174e4c70e1d0e
alex <alex.vlissidis@gmail.com>
2021-02-28 00:55:18 -0800

    Integrate API gateway with eureka

--- gateway-server/src/main/java/com/estiasi/gateway/App.java
+++ gateway-server/src/main/java/com/estiasi/gateway/App.java
@@ -2,9 +2,10 @@ package com.estiasi.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
